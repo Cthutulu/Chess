628e4404db82d1b120a05e7e18d100ae4c8dab4e
jonasberthelsen01 <jonasberthelsen01@gmail.com>
2026-05-12 10:32:24 +0200

    12.05.26

---
+++ homemade.py
@@ -0,0 +1,29 @@
+"""
+Some example classes for people who want to create a homemade bot.
+
+With these classes, bot makers will not have to implement the UCI or XBoard interfaces themselves.
+"""
+import chess
+from chess.engine import PlayResult, Limit
+import random
+from lib.engine_wrapper import MinimalEngine
+from lib.lichess_types import MOVE, HOMEMADE_ARGS_TYPE
+import logging
+
+
+# Use this logger variable to print messages to the console or log files.
+# logger.info("message") will always print "message" to the console or log file.
+# logger.debug("message") will only print "message" if verbose logging is enabled.
+logger = logging.getLogger(__name__)
+
+
+class ExampleEngine(MinimalEngine):
+    """An example engine that all homemade engines inherit."""
+
+
+class RandomMove(ExampleEngine):
+    """Get a random move."""
+
+    def search(self, board: chess.Board, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:  # noqa: ARG002
+        """Choose a random move."""
+        return PlayResult(random.choice(list(board.legal_moves)), None)
