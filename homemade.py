96e7e17bced368ac1543c02a8ff5ff2beaf43cfc
jonas <jonasberthelsen01@gmail.com>
2026-06-07 19:56:41 +0200

    04.06.26

--- homemade.py
+++ homemade.py
@@ -20,7 +20,6 @@ import time
 # logger.debug("message") will only print "message" if verbose logging is enabled.
 logger = logging.getLogger(__name__)
 
-
 class ExampleEngine(MinimalEngine):
     """An example engine that all homemade engines inherit."""
 
